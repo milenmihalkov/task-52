0d088959c9c83477862514a160326237fcb37e7d
Milen <milenmihalkov@gmail.com>
2023-01-11 14:54:55 +0200

    Dynamic article

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,23 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
+  document.body.addEventListener("click", () => {
+    
+    let sampleText = 'Sample Text';
+    const  elements = 5;
+    for(let i=0;i<elements;i++)
+    {
+        let newarticle = document.createElement('article');
+        let articleIdName = 'article-'+i;
+
+        newarticle.setAttribute('id',articleIdName);
+        document.body.appendChild(newarticle);
+        
+        document.getElementById(articleIdName).classList.add('message');
+        document.getElementById(articleIdName).innerHTML = sampleText;
+    }
+
+    
   });
+
 });
